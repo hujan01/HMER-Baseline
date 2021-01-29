282c130a7e06f1bb39bce9c42d3cc56c3a8193ef
jianh1994 <595495856@qq.com>
2021-01-29 16:38:55 +0800

    normal

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 '''
 @Author: your name
 @Date: 2020-01-06 15:08:07
-LastEditTime: 2021-01-11 11:01:17
+LastEditTime: 2021-01-29 15:31:09
 LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /HMER/config.py
@@ -10,13 +10,13 @@ LastEditors: Please set LastEditors
 class Config():  
     seed = 2020
     
-    datasets = ['data/train.pkl', 'data/label/train_caption.txt']
-    valid_datasets = ['data/valid.pkl', 'data/label/test_caption_2014.txt']
+    datasets = ['data/train_v3.pkl', 'data/label/train_caption.txt']
+    valid_datasets = ['data/valid_v3.pkl', 'data/label/test_caption_2014.txt']
     dictionaries = 'data/dictionary.txt'
 
     batch_Imagesize = 500000
     valid_batch_Imagesize = 500000 
-    maxImagesize = 100000
+    maxImagesize = 200000
 
     maxlen = 70
     hidden_size = 256
